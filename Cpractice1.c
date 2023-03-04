9eea6993c9dd8875f6350fcff5b240d25ba98bb8
Michael <michaelelsa12@gmail.com>
2023-03-04 17:27:27 +0300

    c_practice_1

---
+++ Cpractice1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int input(char str[], int n)
+{
+    int ch, i = 0;
+    while ((ch = getchar()) != '\n')
+    if (i < n)
+    str[i++] = ch;
+    str[i] = '\0';
+    return i;
+}
+
+int main()
+{
+    char str[50];
+    int n = input(str, 12);
+
+    printf("%d %s", n, str);
+
+    return 0;
+}
